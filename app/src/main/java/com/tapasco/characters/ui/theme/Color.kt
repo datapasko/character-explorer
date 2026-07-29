de9c48776835d929cb0bf378c001bded7837ad57
Daniel Tapasco <tapaskojd@gmail.com>
2026-07-29 22:44:44 +0200

    feat: application theme

--- app/src/main/java/com/tapasco/characters/ui/theme/Color.kt
+++ app/src/main/java/com/tapasco/characters/ui/theme/Color.kt
@@ -2,10 +2,21 @@ package com.tapasco.characters.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val InterdimensionalGreen = Color(0xFF97CE4C)
+val InterdimensionalYellow = Color(0xFFF0E14A)
+val InterdimensionalMagenta = Color(0xFFB91D8D)
+val InterdimensionalNeutral = Color(0xFF1A1A1E)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+internal val LightBackground = Color(0xFFF8F8F2)
+internal val LightSurface = Color(0xFFFFFFFF)
+internal val LightSurfaceVariant = Color(0xFFE5E7DC)
+internal val LightOutline = Color(0xFF74766D)
+
+internal val DarkSurface = Color(0xFF202024)
+internal val DarkSurfaceVariant = Color(0xFF2B2B30)
+internal val DarkOnSurface = Color(0xFFF1F1F4)
+internal val DarkOutline = Color(0xFF92929A)
+
+internal val OnGreen = Color(0xFF172109)
+internal val OnYellow = Color(0xFF211F00)
+internal val OnMagenta = Color(0xFFFFFFFF)
