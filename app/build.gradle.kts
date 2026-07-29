599edd5557522dc116c909933f501143cedd7113
Daniel Tapasco <tapaskojd@gmail.com>
2026-07-29 19:54:01 +0200

    feat: add Retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,16 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
 
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.serialization.json)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+
     //DI
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
