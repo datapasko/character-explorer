8b82d6be3e8a3f12e92364d9fcdab55fa4f1ac5a
Daniel Tapasco <tapaskojd@gmail.com>
2026-07-29 19:45:57 +0200

    feat: add Koin dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,12 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    //DI
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
+
+    //Test
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
